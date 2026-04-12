9c18b02cdd3d56b95f1a7db9ed0abc3f2e77798f
victorbsad <105154@aluno.uricer.edu.br>
2026-04-12 14:15:22 -0300

    chore: change app window size and name to Lumina

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"flutter_application_1", origin, size)) {
+  Win32Window::Size size(1024, 576);
+  if (!window.Create(L"Lumina", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
